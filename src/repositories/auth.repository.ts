84227f4077377e8d9e35f52f7991f3bc901a5f6f
HieuTrann18 <hieu.tt.2455@aptechlearning.edu.vn>
2026-02-24 13:54:14 +0700

    update

---
+++ src/repositories/auth.repository.ts
@@ -0,0 +1,16 @@
+class AuthRepository {
+  // createRefreshToken(data)
+  // findRefreshToken(token: string)
+  // revokeRefreshToken(token: string)
+  // revokeAllUserTokens(userId: string)
+  // // ===== Session =====
+  // createSession(data)
+  // findSessionById(id: string)
+  // invalidateSession(id: string)
+  // // ===== OTP =====
+  // createOTP(data)
+  // findValidOTP(userId: string, code: string)
+  // markOTPUsed(id: string)
+}
+
+export default new AuthRepository()
